50369cdae7b2da6a74ead8c331f5a312b71242aa
hsuhoche <hsuhoche@gmail.com>
2026-04-02 00:02:32 +0800

    Update menuData.js

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -3,16 +3,19 @@ const menuData = [
     id: 1,
     title: "涼麵",
     items: [
-      { name: "牛排", price: 350 },
-      { name: "義大利麵", price: 250 }
+      { name: "麻醬涼麵", price: 60 },
+      { name: "日式涼麵", price: 65 },
+      { name: "泰式涼麵", price: 65 }
     ]
   },
   {
     id: 2,
-    title: "飲料",
+    title: "鹹粿",
     items: [
-      { name: "紅茶", price: 50 },
-      { name: "咖啡", price: 80 }
+      { name: "煎鹹粿", price: 45 },
+      { name: "蛋包鹹粿", price: 55 },
+      { name: "炒鹹粿角", price: 70 },
+      { name: "鹹粿湯", price: 70 }
     ]
   }
 ];
